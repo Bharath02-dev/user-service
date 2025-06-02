ea7cc8d4e7beba27f27c356358ee0a86578bc277
Bharath02-dev <bharathwork02@gmail.com>
2025-06-02 20:48:08 +0530

    Refactered

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/health-check")
-    public String healthCheck(){
-        return "Ok";
+    @GetMapping("/health-check-test")
+    public ResponseEntity<String> healthCheck(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
